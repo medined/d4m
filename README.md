e9e381d9024f23d2f44e0cffed36e61d4d7e9750
David <david.medinets@gmail.com>
2014-12-06 20:53:27 -0500

    Add cobertura to pom

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-d4m
-===
+# D4M
 
-Java support for the D4M Accumulo schema
+[![Build Status](https://travis-ci.org/medined/d4m.svg?branch=master)](https://travis-ci.org/medined/D4M_Schema) - Travis CI
+<br/>
+[![Build Status](https://api.shippable.com/projects/5385e15ce5ca5fba01621831/badge/master)](https://www.shippable.com/projects/5385e15ce5ca5fba01621831) - Shippable
+<br/>
+[![Coverage Status](https://coveralls.io/repos/medined/d4m/badge.png?branch=master)](https://coveralls.io/r/medined/D4M_Schema?branch=master)
+<br/>
+
+# Java Support for D4M Accumulo Schema
+
---
+++ src/test/java/com/codebits/d4m/TableManagerTest.java
@@ -0,0 +1,146 @@
+package com.codebits.d4m;
+
+import java.util.SortedSet;
+import java.util.concurrent.TimeUnit;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.BatchWriter;
+import org.apache.accumulo.core.client.BatchWriterConfig;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.client.TableExistsException;
+import org.apache.accumulo.core.client.TableNotFoundException;
+import org.apache.accumulo.core.client.admin.TableOperations;
+import org.apache.accumulo.core.data.Mutation;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class TableManagerTest {
+
+    Connector mockConnector = mock(Connector.class);
+    TableOperations mockTableOperations = mock(TableOperations.class);
+
+    private TableManager instance = null;
+
+    @Before
+    public void setup() {
+        instance = new TableManager();
+        instance.setConnector(mockConnector);
+        instance.setTableOperations(mockTableOperations);
+    }
+
+    @Test
+    public void testCreateTables() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.FALSE);
+
+        BatchWriterConfig bwConfig = new BatchWriterConfig();
+        bwConfig.setMaxLatency(10000, TimeUnit.MINUTES);
+        bwConfig.setMaxMemory(10000000);
+        bwConfig.setMaxWriteThreads(5);
+        bwConfig.setTimeout(5, TimeUnit.MINUTES);
+
+        BatchWriter mockBatchWriter = mock(BatchWriter.class);
+        when(mockConnector.createBatchWriter("TedgeMetadata", bwConfig)).thenReturn(mockBatchWriter);
+
+        instance.createTables();
+        verify(mockTableOperations, times(5)).exists(any(String.class));
+        verify(mockTableOperations).create("Tedge");
+        verify(mockTableOperations).create("TedgeTranspose");
+        verify(mockTableOperations).create("TedgeDegree");
+        verify(mockTableOperations).create("TedgeMetadata");
+        verify(mockTableOperations).create("TedgeText");
+        verify(mockTableOperations).attachIterator(matches("TedgeDegree"), any(IteratorSetting.class));
+        verify(mockTableOperations).attachIterator(matches("TedgeMetadata"), any(IteratorSetting.class));
+        verifyNoMoreInteractions(mockTableOperations);
+
+        verify(mockBatchWriter).addMutation(any(Mutation.class));
+        verify(mockBatchWriter).close();
+        verifyNoMoreInteractions(mockBatchWriter);
+    }
+
+    @Test
+    public void testAddSplitsForSha1() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        instance.addSplitsForSha1();
+        verify(mockTableOperations).addSplits(matches("Tedge"), any(SortedSet.class));
+        verify(mockTableOperations).addSplits(matches("TedgeText"), any(SortedSet.class));
+        verifyNoMoreInteractions(mockTableOperations);
+    }
+
+    @Test
+    public void testCreateTables_with_all_existing_does_nothing() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.TRUE);
+        instance.createTables();
+        verify(mockTableOperations).exists("Tedge");
+        verify(mockTableOperations).exists("TedgeTranspose");
+        verify(mockTableOperations).exists("TedgeDegree");
+        verify(mockTableOperations).exists("TedgeMetadata");
+        verify(mockTableOperations).exists("TedgeText");
+        verifyNoMoreInteractions(mockTableOperations);
+    }
+
+    @Test(expected = D4MException.class)
+    public void testCreateTables_with_just_edge_table() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.FALSE);
+        instance.createTables();
+    }
+
+    @Test(expected = D4MException.class)
+    public void testCreateTables_with_just_transpose_table() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.FALSE);
+        instance.createTables();
+    }
+
+    @Test(expected = D4MException.class)
+    public void testCreateTables_with_just_degree_table() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.FALSE);
+        instance.createTables();
+    }
+
+    @Test(expected = D4MException.class)
+    public void testCreateTables_with_just_field_table() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.TRUE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.FALSE);
+        instance.createTables();
+    }
+
+    @Test(expected = D4MException.class)
+    public void testCreateTables_with_just_text_table() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        when(mockTableOperations.exists("Tedge")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeTranspose")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeDegree")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeMetadata")).thenReturn(Boolean.FALSE);
+        when(mockTableOperations.exists("TedgeText")).thenReturn(Boolean.TRUE);
+        instance.createTables();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateTables_with_null_tableOperation() throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException {
+        instance.setTableOperations(null);
+        instance.createTables();
+    }
+
+}
